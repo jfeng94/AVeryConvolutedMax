64eb7be300436fc14e89b7a36a26a820c9ffb18b
John Li <johnzli1995@gmail.com>
2015-05-30 01:26:38 -0700

    Also made some small edits to camera.cpp and camera.h (such as guards and stuff)

--- src/camera.h
+++ src/camera.h
@@ -1,7 +1,15 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include "point.h"
+
 class Camera
 {
     private:
         float * R, * G, * B;
         int width, height;
 
+    public:
+
 };
+#endif // CAMERA_H_
